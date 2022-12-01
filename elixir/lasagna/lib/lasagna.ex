2f092387b3dffd0713f8507ef009036ee581943a
SaberCon <894876785@qq.com>
2022-12-01 18:28:41 +0800

    Finish elixir lasagna

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,11 +1,17 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
+  def expected_minutes_in_oven(), do: 40
 
-  # Please define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(spent_minutes) do
+    expected_minutes_in_oven() - spent_minutes
+  end
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
 
-  # Please define the 'total_time_in_minutes/2' function
+  def total_time_in_minutes(layers, spent_minutes) do
+    preparation_time_in_minutes(layers) + spent_minutes
+  end
 
-  # Please define the 'alarm/0' function
+  def alarm(), do: "Ding!"
 end
