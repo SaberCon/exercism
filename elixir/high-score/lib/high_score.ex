842a98a101c312c7480b6372888f3f895517a740
SaberCon <894876785@qq.com>
2022-12-02 14:58:44 +0800

    Finish elixir high-score

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,25 +1,25 @@
 defmodule HighScore do
-  def new() do
-    # Please implement the new/0 function
-  end
+  @default_score 0
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
+  def new(), do: %{}
+
+  def add_player(scores, name, score \\ @default_score) do
+    Map.put(scores, name, score)
   end
 
   def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
+    Map.delete(scores, name)
   end
 
   def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
+    Map.put(scores, name, @default_score)
   end
 
   def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
+    Map.update(scores, name, @default_score + score, &(&1 + score))
   end
 
   def get_players(scores) do
-    # Please implement the get_players/1 function
+    Map.keys(scores)
   end
 end
