1bcdc31c353980b3aac96d126a6ae7a21194ec22
SaberCon <894876785@qq.com>
2022-12-02 18:09:58 +0800

    Finish elixir take-a-number

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,23 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(&loop/0)
+  end
+
+  defp loop(state \\ 0) do
+    receive do
+      {:report_state, sender} -> report_state(state, sender)
+      {:take_a_number, sender} -> take_a_number(state, sender)
+      :stop -> :ok
+      _ -> loop(state)
+    end
+  end
+
+  defp report_state(state, sender) do
+    send(sender, state)
+    loop(state)
+  end
+
+  defp take_a_number(state, sender) do
+    report_state(state + 1, sender)
   end
 end
