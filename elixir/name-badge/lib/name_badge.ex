aa3d3a12e3d065bb05c171e317fd6754341ca2c0
SaberCon <894876785@qq.com>
2022-12-02 17:40:17 +0800

    Finish elixir name-badge

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,5 +1,9 @@
 defmodule NameBadge do
+  def print(id, name, nil), do: print(id, name, "OWNER")
+
   def print(id, name, department) do
-    # Please implement the print/3 function
+    [if(id, do: "[#{id}]"), name, String.upcase(department)]
+    |> Enum.reject(&is_nil/1)
+    |> Enum.join(" - ")
   end
 end
