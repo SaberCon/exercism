6ca2a7def4da849b1280c340676cc8a55cd7ea83
SaberCon <894876785@qq.com>
2022-12-03 17:54:51 +0800

    Finish elixir newsletter

--- elixir/newsletter/lib/newsletter.ex
+++ elixir/newsletter/lib/newsletter.ex
@@ -1,21 +1,28 @@
 defmodule Newsletter do
   def read_emails(path) do
-    # Please implement the read_emails/1 function
+    File.read!(path)
+    |> String.split()
   end
 
   def open_log(path) do
-    # Please implement the open_log/1 function
+    File.open!(path, [:write])
   end
 
   def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
+    IO.puts(pid, email)
   end
 
   def close_log(pid) do
-    # Please implement the close_log/1 function
+    File.close(pid)
   end
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    log = open_log(log_path)
+
+    for email <- read_emails(emails_path),
+        send_fun.(email) == :ok,
+        do: log_sent_email(log, email)
+
+    close_log(log)
   end
 end
