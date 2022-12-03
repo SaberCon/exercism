a6836a7b530eb78968e5662a33fa869167d032db
SaberCon <894876785@qq.com>
2022-12-03 13:06:28 +0800

    Finish elixir basketball-website

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,10 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    String.split(path, ".")
+    |> Enum.reduce(data, fn key, acc -> acc[key] end)
   end
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    get_in(data, String.split(path, "."))
   end
 end
