de1611c8a710bbfe47a4f232531be61bf767976f
SaberCon <894876785@qq.com>
2022-12-04 10:13:19 +0800

    Finish elixir rpn-calculator

--- elixir/rpn-calculator/lib/rpn_calculator.ex
+++ elixir/rpn-calculator/lib/rpn_calculator.ex
@@ -1,13 +1,17 @@
 defmodule RPNCalculator do
   def calculate!(stack, operation) do
-    # Please implement the calculate!/2 function
+    operation.(stack)
   end
 
   def calculate(stack, operation) do
-    # Please implement the calculate/2 function
+    {:ok, calculate!(stack, operation)}
+  rescue
+    _ -> :error
   end
 
   def calculate_verbose(stack, operation) do
-    # Please implement the calculate_verbose/2 function
+    {:ok, calculate!(stack, operation)}
+  rescue
+    e -> {:error, e.message}
   end
 end
