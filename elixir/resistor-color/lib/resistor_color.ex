3a4f7875e43be0113e327eb56fcabb40c57adc29
SaberCon <894876785@qq.com>
2022-12-04 20:14:12 +0800

    Finish elixir resistor-color

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -1,8 +1,11 @@
 defmodule ResistorColor do
+  @colors ~w(black brown red orange yellow green blue violet grey white)a
+
   @doc """
   Return the value of a color band
   """
   @spec code(atom) :: integer()
   def code(color) do
+    Enum.find_index(@colors, &(&1 == color))
   end
 end
