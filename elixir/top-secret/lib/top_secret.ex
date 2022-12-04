b99af6e9e9c319f2abc40bdb6fe3db8a85063f00
SaberCon <894876785@qq.com>
2022-12-04 15:12:25 +0800

    Finish elixir top-secret

--- elixir/top-secret/lib/top_secret.ex
+++ elixir/top-secret/lib/top_secret.ex
@@ -1,13 +1,36 @@
 defmodule TopSecret do
   def to_ast(string) do
-    # Please implement the to_ast/1 function
+    Code.string_to_quoted!(string)
+  end
+
+  def decode_secret_message_part({op, _, [fun_part | _]} = ast, acc)
+      when op in [:def, :defp] do
+    {ast, decode_function_part(fun_part, acc)}
   end
 
   def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+    {ast, acc}
+  end
+
+  defp decode_function_part({:when, _, [fun | _]}, acc) do
+    decode_function_part(fun, acc)
+  end
+
+  defp decode_function_part({name, _, args}, acc) do
+    [decode_function(name, args) | acc]
+  end
+
+  defp decode_function(name, args) do
+    name
+    |> to_string()
+    |> String.slice(0, length(args || []))
   end
 
   def decode_secret_message(string) do
-    # Please implement the decode_secret_message/1 function
+    to_ast(string)
+    |> Macro.prewalk([], &decode_secret_message_part/2)
+    |> elem(1)
+    |> Enum.reverse()
+    |> Enum.join()
   end
 end
