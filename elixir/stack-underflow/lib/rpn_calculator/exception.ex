d1eb46f6bab9d052be0d8f90ce99e233ff39f297
SaberCon <894876785@qq.com>
2022-12-04 11:06:26 +0800

    Finish elixir stack-underflow

--- elixir/stack-underflow/lib/rpn_calculator/exception.ex
+++ elixir/stack-underflow/lib/rpn_calculator/exception.ex
@@ -1,5 +1,27 @@
 defmodule RPNCalculator.Exception do
-  # Please implement DivisionByZeroError here.
+  defmodule DivisionByZeroError do
+    defexception message: "division by zero occurred"
+  end
 
-  # Please implement StackUnderflowError here.
+  defmodule StackUnderflowError do
+    defexception message: "stack underflow occurred"
+
+    @impl true
+    def exception([]) do
+      %__MODULE__{}
+    end
+
+    @impl true
+    def exception(term) do
+      %__MODULE__{message: "stack underflow occurred, context: #{term}"}
+    end
+  end
+
+  def divide(stack) do
+    case stack do
+      [0, _] -> raise DivisionByZeroError
+      [divisor, dividend] -> dividend / divisor
+      _ -> raise StackUnderflowError, "when dividing"
+    end
+  end
 end
