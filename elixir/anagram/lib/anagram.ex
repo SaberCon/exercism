874f090bbb5eaa2721b6e4fe1951b4280a1e3db9
SaberCon <894876785@qq.com>
2022-12-05 16:29:38 +0800

    Finish elixir anagram

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,5 +4,12 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    base = String.downcase(base)
+
+    Enum.filter(candidates, &anagram?(String.downcase(&1), base))
   end
+
+  defp anagram?(a, b), do: a != b and sorted(a) == sorted(b)
+
+  defp sorted(string), do: string |> to_charlist() |> Enum.sort()
 end
