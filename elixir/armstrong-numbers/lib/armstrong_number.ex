dfbe50b6e7f03d8ce5665e25a86fa4cd7db5dc75
SaberCon <894876785@qq.com>
2022-12-05 16:42:53 +0800

    Finish elixir armstrong-numbers

--- elixir/armstrong-numbers/lib/armstrong_number.ex
+++ elixir/armstrong-numbers/lib/armstrong_number.ex
@@ -5,5 +5,11 @@ defmodule ArmstrongNumber do
 
   @spec valid?(integer) :: boolean
   def valid?(number) do
+    digits = Integer.digits(number)
+    len = length(digits)
+
+    digits
+    |> Enum.reduce(0, fn d, acc -> acc + Integer.pow(d, len) end)
+    |> Kernel.==(number)
   end
 end
