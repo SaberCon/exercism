d9191ed06b3a7a2bca14b9ffc08dd94ab1113edd
SaberCon <894876785@qq.com>
2022-12-05 17:46:01 +0800

    Finish elixir collatz-conjecture

--- elixir/collatz-conjecture/lib/collatz_conjecture.ex
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -1,4 +1,6 @@
 defmodule CollatzConjecture do
+  import Integer, only: [is_even: 1, is_odd: 1]
+
   @doc """
   calc/1 takes an integer and returns the number of steps required to get the
   number to 1 when following the rules:
@@ -6,6 +8,13 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(input) do
+  def calc(input) when is_integer(input) and input > 0 do
+    input
+    |> Stream.iterate(fn
+      n when is_odd(n) -> n * 3 + 1
+      n when is_even(n) -> div(n, 2)
+    end)
+    |> Stream.take_while(fn n -> n > 1 end)
+    |> Enum.count()
   end
 end
