a7f03bf5c304fcaf93cf9cab0161dfe5abc256d8
SaberCon <894876785@qq.com>
2022-12-06 15:20:25 +0800

    Finish elixir pig-latin

--- elixir/pig-latin/lib/pig_latin.ex
+++ elixir/pig-latin/lib/pig_latin.ex
@@ -4,5 +4,20 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    phrase
+    |> String.split()
+    |> Enum.map_join(" ", &translate_word/1)
+  end
+
+  defp translate_word(word) do
+    consonants = first_consonants(word)
+    rest = String.replace_prefix(word, consonants, "")
+    rest <> consonants <> "ay"
+  end
+
+  defp first_consonants(word) do
+    ~r/^(qu|[xy](?=[aeiou])|[^aeiouxy])*/
+    |> Regex.run(word)
+    |> hd()
   end
 end
