57ee0ade6fa18c7ffa84e4d7afdd8e5a0c29fb83
SaberCon <894876785@qq.com>
2022-12-06 15:48:31 +0800

    Finish elixir protein-translation

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,10 +1,43 @@
 defmodule ProteinTranslation do
+  @codon_to_protein_map %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
   @doc """
-  Given an RNA string, return a list of proteins specified by codons, in order.
+  Given an RNA string, return a list of acc specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
   def of_rna(rna) do
+    do_of_rna(rna, [])
   end
+
+  defp do_of_rna("", acc), do: {:ok, Enum.reverse(acc)}
+
+  defp do_of_rna(<<codon::binary-size(3), rest::binary>>, acc) do
+    case of_codon(codon) do
+      {:ok, "STOP"} -> do_of_rna("", acc)
+      {:ok, protein} -> do_of_rna(rest, [protein | acc])
+      {:error, _} -> {:error, "invalid RNA"}
+    end
+  end
+
+  defp do_of_rna(_, _), do: {:error, "invalid RNA"}
 
   @doc """
   Given a codon, return the corresponding protein
@@ -29,5 +62,9 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def of_codon(codon) do
+    case @codon_to_protein_map[codon] do
+      nil -> {:error, "invalid codon"}
+      protein -> {:ok, protein}
+    end
   end
 end
