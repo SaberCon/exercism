9ba90c9e822586452c6aa39f9146d0bba327f06c
SaberCon <894876785@qq.com>
2022-12-06 16:20:03 +0800

    Finish elixir resistor-color-duo

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -1,8 +1,26 @@
 defmodule ResistorColorDuo do
+  @band_colors [
+    :black,
+    :brown,
+    :red,
+    :orange,
+    :yellow,
+    :green,
+    :blue,
+    :violet,
+    :grey,
+    :white
+  ]
+
   @doc """
   Calculate a resistance value from two colors
   """
   @spec value(colors :: [atom]) :: integer
-  def value(colors) do
+  def value([color1, color2 | _]) do
+    get_value(color1) * 10 + get_value(color2)
+  end
+
+  defp get_value(color) do
+    Enum.find_index(@band_colors, fn c -> c == color end)
   end
 end
