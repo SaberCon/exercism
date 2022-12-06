ab2f1a518b2576db21319e20b23fd4c54bbe4314
SaberCon <894876785@qq.com>
2022-12-06 11:20:52 +0800

    Finish elixir pangram

--- elixir/pangram/lib/pangram.ex
+++ elixir/pangram/lib/pangram.ex
@@ -1,4 +1,6 @@
 defmodule Pangram do
+  @alphabet MapSet.new(?a..?z)
+
   @doc """
   Determines if a word or sentence is a pangram.
   A pangram is a sentence using every letter of the alphabet at least once.
@@ -11,8 +13,13 @@ defmodule Pangram do
       true
 
   """
-
   @spec pangram?(String.t()) :: boolean
   def pangram?(sentence) do
+    sentence
+    |> String.downcase()
+    |> to_charlist()
+    |> Stream.filter(fn c -> c in @alphabet end)
+    |> Stream.uniq()
+    |> Enum.count() == MapSet.size(@alphabet)
   end
 end
