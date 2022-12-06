f7209d87218d68ad0b2b5d1a629fa426aa51ffd8
SaberCon <894876785@qq.com>
2022-12-06 18:46:23 +0800

    Finish elixir scrabble-score

--- elixir/scrabble-score/lib/scrabble.ex
+++ elixir/scrabble-score/lib/scrabble.ex
@@ -4,5 +4,23 @@ defmodule Scrabble do
   """
   @spec score(String.t()) :: non_neg_integer
   def score(word) do
+    word
+    |> String.upcase()
+    |> to_charlist()
+    |> Enum.map(&value/1)
+    |> Enum.sum()
+  end
+
+  defp value(letter) do
+    cond do
+      letter in 'AEIOULNRST' -> 1
+      letter in 'DG' -> 2
+      letter in 'BCMP' -> 3
+      letter in 'FHVWY' -> 4
+      letter in 'K' -> 5
+      letter in 'JX' -> 8
+      letter in 'QZ' -> 10
+      true -> 0
+    end
   end
 end
