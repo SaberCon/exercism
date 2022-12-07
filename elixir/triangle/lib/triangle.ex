a3e25838938599dfab535133b6da8a3be5bff1db
SaberCon <894876785@qq.com>
2022-12-08 00:01:06 +0800

    Finish elixir triangle

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -6,5 +6,13 @@ defmodule Triangle do
   """
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
   def kind(a, b, c) do
+    case Enum.sort([a, b, c]) do
+      [n, _, _] when n <= 0 -> {:error, "all side lengths must be positive"}
+      [n1, n2, n3] when n1 + n2 < n3 -> {:error, "side lengths violate triangle inequality"}
+      [n, n, n] -> {:ok, :equilateral}
+      [_, n, n] -> {:ok, :isosceles}
+      [n, n, _] -> {:ok, :isosceles}
+      _ -> {:ok, :scalene}
+    end
   end
 end
