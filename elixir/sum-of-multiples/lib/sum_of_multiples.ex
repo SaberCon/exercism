73be08f2071fbdaaf80d8b880191df77f6932f03
SaberCon <894876785@qq.com>
2022-12-07 23:44:43 +0800

    Finish elixir sum-of-multiples

--- elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -4,5 +4,17 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    nums(MapSet.new(), limit, factors) |> Enum.sum()
+  end
+
+  defp nums(acc, _, []), do: acc
+
+  defp nums(acc, limit, [0 | rest]), do: nums(acc, limit, rest)
+
+  defp nums(acc, limit, [factor | rest]) do
+    factor..(limit - 1)//factor
+    |> MapSet.new()
+    |> MapSet.union(acc)
+    |> nums(limit, rest)
   end
 end
