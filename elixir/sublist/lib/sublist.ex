4d79a2eca4a5efc03e6fe21415fc5755c48cc335
SaberCon <894876785@qq.com>
2022-12-07 23:28:47 +0800

    Finish elixir sublist

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -4,5 +4,18 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
+    cond do
+      a == b -> :equal
+      superlist?(a, b) -> :superlist
+      superlist?(b, a) -> :sublist
+      true -> :unequal
+    end
+  end
+
+  defp superlist?(_, []), do: true
+  defp superlist?([], _), do: false
+
+  defp superlist?(a, b) do
+    List.starts_with?(a, b) or superlist?(tl(a), b)
   end
 end
