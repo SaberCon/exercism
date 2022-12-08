2b0708d5a0c2b0002737c0f3656bb60de0cc3e50
SaberCon <894876785@qq.com>
2022-12-08 12:00:52 +0800

    Finish elixir binary-search-tree

--- elixir/binary-search-tree/lib/binary_search_tree.ex
+++ elixir/binary-search-tree/lib/binary_search_tree.ex
@@ -6,15 +6,27 @@ defmodule BinarySearchTree do
   """
   @spec new(any) :: bst_node
   def new(data) do
-    # Your implementation here
+    %{data: data, left: nil, right: nil}
   end
 
   @doc """
   Creates and inserts a node with its value as 'data' into the tree.
   """
   @spec insert(bst_node, any) :: bst_node
-  def insert(tree, data) do
-    # Your implementation here
+  def insert(%{data: value, left: nil} = tree, data) when value >= data do
+    %{tree | left: new(data)}
+  end
+
+  def insert(%{data: value, left: left} = tree, data) when value >= data do
+    %{tree | left: insert(left, data)}
+  end
+
+  def insert(%{right: nil} = tree, data) do
+    %{tree | right: new(data)}
+  end
+
+  def insert(%{right: right} = tree, data) do
+    %{tree | right: insert(right, data)}
   end
 
   @doc """
@@ -22,6 +34,15 @@ defmodule BinarySearchTree do
   """
   @spec in_order(bst_node) :: [any]
   def in_order(tree) do
-    # Your implementation here
+    traverse_reversed([], tree) |> Enum.reverse()
+  end
+
+  defp traverse_reversed(acc, nil), do: acc
+
+  defp traverse_reversed(acc, tree) do
+    acc
+    |> traverse_reversed(tree.left)
+    |> List.insert_at(0, tree.data)
+    |> traverse_reversed(tree.right)
   end
 end
