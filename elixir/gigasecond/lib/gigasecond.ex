3e7be6cfe62eceb3b15b2cb39304f0c7418d4a00
SaberCon <894876785@qq.com>
2022-12-08 22:46:18 +0800

    Finish elixir gigasecond

--- elixir/gigasecond/lib/gigasecond.ex
+++ elixir/gigasecond/lib/gigasecond.ex
@@ -1,9 +1,15 @@
 defmodule Gigasecond do
+  @gigasecond Integer.pow(10, 9)
+
   @doc """
   Calculate a date one billion seconds after an input date.
   """
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
           {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
-  def from({{year, month, day}, {hours, minutes, seconds}}) do
+  def from(erl_datetime) do
+    erl_datetime
+    |> NaiveDateTime.from_erl!()
+    |> NaiveDateTime.add(@gigasecond)
+    |> NaiveDateTime.to_erl()
   end
 end
