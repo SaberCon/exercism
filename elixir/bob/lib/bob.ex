15ec39095c87dc1cf45c5228641eac42b06cdbed
SaberCon <894876785@qq.com>
2022-12-08 12:58:42 +0800

    Finish elixir bob

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,5 +1,18 @@
 defmodule Bob do
   @spec hey(String.t()) :: String.t()
   def hey(input) do
+    input = String.trim(input)
+
+    cond do
+      question?(input) and yell?(input) -> "Calm down, I know what I'm doing!"
+      question?(input) -> "Sure."
+      yell?(input) -> "Whoa, chill out!"
+      silence?(input) -> "Fine. Be that way!"
+      true -> "Whatever."
+    end
   end
+
+  defp silence?(input), do: input == ""
+  defp question?(input), do: String.last(input) == "?"
+  defp yell?(input), do: String.upcase(input) == input and String.downcase(input) != input
 end
