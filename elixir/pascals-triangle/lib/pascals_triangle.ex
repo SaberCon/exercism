79e91b233f099fc4aaa272461e9e19c2debb8213
SaberCon <894876785@qq.com>
2022-12-09 21:32:35 +0800

    Finish elixir pascals-triangle

--- elixir/pascals-triangle/lib/pascals_triangle.ex
+++ elixir/pascals-triangle/lib/pascals_triangle.ex
@@ -5,5 +5,14 @@ defmodule PascalsTriangle do
   """
   @spec rows(integer) :: [[integer]]
   def rows(num) do
+    Stream.iterate([1], &next_row/1)
+    |> Enum.take(num)
+  end
+
+  defp next_row(row) do
+    row
+    |> Enum.chunk_every(2, 1)
+    |> Enum.map(&Enum.sum/1)
+    |> List.insert_at(0, 1)
   end
 end
