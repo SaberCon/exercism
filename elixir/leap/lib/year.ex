a727adaefeac4e48ca972103c7e7b6f6a6af9056
SaberCon <894876785@qq.com>
2022-12-09 15:51:46 +0800

    Finish elixir leap

--- elixir/leap/lib/year.ex
+++ elixir/leap/lib/year.ex
@@ -1,4 +1,6 @@
 defmodule Year do
+  defguardp evenly_divisible?(dividend, divisor) when rem(dividend, divisor) == 0
+
   @doc """
   Returns whether 'year' is a leap year.
 
@@ -9,6 +11,7 @@ defmodule Year do
       unless the year is also evenly divisible by 400
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) do
-  end
+  def leap_year?(year) when evenly_divisible?(year, 400), do: true
+  def leap_year?(year) when evenly_divisible?(year, 4), do: not evenly_divisible?(year, 100)
+  def leap_year?(_), do: false
 end
