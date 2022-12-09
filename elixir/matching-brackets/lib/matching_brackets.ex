eb1ccd642d0adaadcc1fe7605fa8701878b9e2e2
SaberCon <894876785@qq.com>
2022-12-09 16:38:19 +0800

    Finish elixir matching-brackets

--- elixir/matching-brackets/lib/matching_brackets.ex
+++ elixir/matching-brackets/lib/matching_brackets.ex
@@ -3,6 +3,22 @@ defmodule MatchingBrackets do
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
   @spec check_brackets(String.t()) :: boolean
-  def check_brackets(str) do
-  end
+  def check_brackets(str), do: matched?(str, [])
+
+  defp matched?("", []), do: true
+
+  defp matched?("", _), do: false
+
+  defp matched?(<<head, str::binary>>, brackets) when head in '[{(',
+    do: matched?(str, [head | brackets])
+
+  defp matched?(<<?], str::binary>>, [?[ | brackets]), do: matched?(str, brackets)
+
+  defp matched?(<<?}, str::binary>>, [?{ | brackets]), do: matched?(str, brackets)
+
+  defp matched?(<<?), str::binary>>, [?( | brackets]), do: matched?(str, brackets)
+
+  defp matched?(<<head, _::binary>>, _) when head in ']})', do: false
+
+  defp matched?(<<_, str::binary>>, brackets), do: matched?(str, brackets)
 end
