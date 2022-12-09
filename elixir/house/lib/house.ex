26533cd4e18ca3d1f3ea777c793023b1e4fffc5e
SaberCon <894876785@qq.com>
2022-12-09 11:22:25 +0800

    Finish elixir house

--- elixir/house/lib/house.ex
+++ elixir/house/lib/house.ex
@@ -1,8 +1,33 @@
 defmodule House do
+  @clauses {
+    "the house that Jack built",
+    "the malt that lay in",
+    "the rat that ate",
+    "the cat that killed",
+    "the dog that worried",
+    "the cow with the crumpled horn that tossed",
+    "the maiden all forlorn that milked",
+    "the man all tattered and torn that kissed",
+    "the priest all shaven and shorn that married",
+    "the rooster that crowed in the morn that woke",
+    "the farmer sowing his corn that kept",
+    "the horse and the hound and the horn that belonged to"
+  }
+
   @doc """
   Return verses of the nursery rhyme 'This is the House that Jack Built'.
   """
   @spec recite(start :: integer, stop :: integer) :: String.t()
   def recite(start, stop) do
+    start..stop
+    |> Enum.map_join(fn i -> "This is #{verse(i)}.\n" end)
+  end
+
+  defp verse(1) do
+    elem(@clauses, 0)
+  end
+
+  defp verse(index) do
+    "#{elem(@clauses, index - 1)} #{verse(index - 1)}"
   end
 end
