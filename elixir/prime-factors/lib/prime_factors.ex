1888c2e4e32ede34cfb25521d8accee1d0afc998
SaberCon <894876785@qq.com>
2022-12-10 10:23:44 +0800

    Finish elixir prime-factors

--- elixir/prime-factors/lib/prime_factors.ex
+++ elixir/prime-factors/lib/prime_factors.ex
@@ -8,6 +8,14 @@ defmodule PrimeFactors do
   The prime factors of 'number' will be ordered lowest to highest.
   """
   @spec factors_for(pos_integer) :: [pos_integer]
-  def factors_for(number) do
+  def factors_for(1), do: []
+  def factors_for(number), do: factors(number, 2, [])
+
+  defp factors(number, factor, acc) do
+    cond do
+      number < factor * factor -> [number | acc] |> Enum.reverse()
+      rem(number, factor) == 0 -> factors(div(number, factor), factor, [factor | acc])
+      true -> factors(number, factor + 1, acc)
+    end
   end
 end
