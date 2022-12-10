9324e40d48ea5e25d0be59a5486f1e078a657a6e
SaberCon <894876785@qq.com>
2022-12-10 21:02:34 +0800

    Finish elixir complex-numbers

--- elixir/complex-numbers/lib/complex_numbers.ex
+++ elixir/complex-numbers/lib/complex_numbers.ex
@@ -11,21 +11,25 @@ defmodule ComplexNumbers do
   Return the real part of a complex number
   """
   @spec real(a :: complex) :: float
-  def real(a) do
-  end
+  def real({r, _}), do: r
 
   @doc """
   Return the imaginary part of a complex number
   """
   @spec imaginary(a :: complex) :: float
-  def imaginary(a) do
-  end
+  def imaginary({_, i}), do: i
+
+  defp complex_number({r, i}), do: {r, i}
+  defp complex_number(r), do: {r, 0}
 
   @doc """
   Multiply two complex numbers, or a real and a complex number
   """
   @spec mul(a :: complex | float, b :: complex | float) :: complex
   def mul(a, b) do
+    {ar, ai} = complex_number(a)
+    {br, bi} = complex_number(b)
+    {ar * br - ai * bi, ai * br + ar * bi}
   end
 
   @doc """
@@ -33,40 +37,46 @@ defmodule ComplexNumbers do
   """
   @spec add(a :: complex | float, b :: complex | float) :: complex
   def add(a, b) do
+    {ar, ai} = complex_number(a)
+    {br, bi} = complex_number(b)
+    {ar + br, ai + bi}
   end
+
+  defp negative({r, i}), do: {-r, -i}
+  defp negative(r), do: -r
 
   @doc """
   Subtract two complex numbers, or a real and a complex number
   """
   @spec sub(a :: complex | float, b :: complex | float) :: complex
-  def sub(a, b) do
-  end
+  def sub(a, b), do: add(a, negative(b))
 
   @doc """
   Divide two complex numbers, or a real and a complex number
   """
   @spec div(a :: complex | float, b :: complex | float) :: complex
   def div(a, b) do
+    {ar, ai} = complex_number(a)
+    {br, bi} = complex_number(b)
+    divisor = br ** 2 + bi ** 2
+    {(ar * br + ai * bi) / divisor, (ai * br - ar * bi) / divisor}
   end
 
   @doc """
   Absolute value of a complex number
   """
   @spec abs(a :: complex) :: float
-  def abs(a) do
-  end
+  def abs({r, i}), do: :math.sqrt(r ** 2 + i ** 2)
 
   @doc """
   Conjugate of a complex number
   """
   @spec conjugate(a :: complex) :: complex
-  def conjugate(a) do
-  end
+  def conjugate({r, i}), do: {r, -i}
 
   @doc """
   Exponential of a complex number
   """
   @spec exp(a :: complex) :: complex
-  def exp(a) do
-  end
+  def exp({r, i}), do: mul(:math.exp(r), {:math.cos(i), :math.sin(i)})
 end
