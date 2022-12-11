4283aa827a954afd599c84f8e06f30088716ef45
SaberCon <894876785@qq.com>
2022-12-11 09:19:09 +0800

    Finish elixir crypto-square

--- elixir/crypto-square/lib/crypto_square.ex
+++ elixir/crypto-square/lib/crypto_square.ex
@@ -7,6 +7,32 @@ defmodule CryptoSquare do
     "ac bd"
   """
   @spec encode(String.t()) :: String.t()
+  def encode(""), do: ""
+
   def encode(str) do
+    text = normalize(str)
+    cols = get_cols(text)
+    do_encode(text, cols)
+  end
+
+  defp normalize(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/\W/, "")
+  end
+
+  defp get_cols(text) do
+    text
+    |> String.length()
+    |> :math.sqrt()
+    |> ceil()
+  end
+
+  defp do_encode(text, cols) do
+    text
+    |> String.graphemes()
+    |> Enum.chunk_every(cols, cols, Stream.cycle([" "]))
+    |> Enum.zip_with(&Enum.join/1)
+    |> Enum.join(" ")
   end
 end
