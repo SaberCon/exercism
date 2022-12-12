3fad3a302b158db5df5d5a7765fa7eb8cafdd707
SaberCon <894876785@qq.com>
2022-12-12 16:45:10 +0800

    Finish elixir parallel-letter-frequency

--- elixir/parallel-letter-frequency/lib/frequency.ex
+++ elixir/parallel-letter-frequency/lib/frequency.ex
@@ -8,5 +8,25 @@ defmodule Frequency do
   """
   @spec frequency([String.t()], pos_integer) :: map
   def frequency(texts, workers) do
+    texts
+    |> Task.async_stream(&do_frequency/1, max_concurrent: workers)
+    |> Stream.map(fn {:ok, result} -> result end)
+    |> merge()
+  end
+
+  defp do_frequency(text) do
+    text
+    |> String.downcase()
+    |> String.graphemes()
+    |> Stream.filter(&(&1 =~ ~r/[[:alpha:]]/))
+    |> Enum.frequencies()
+  end
+
+  defp merge(frequencies_list) do
+    Enum.reduce(frequencies_list, %{}, &merge/2)
+  end
+
+  defp merge(frequencies1, frequencies2) do
+    Map.merge(frequencies1, frequencies2, fn _k, v1, v2 -> v1 + v2 end)
   end
 end
