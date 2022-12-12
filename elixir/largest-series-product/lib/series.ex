6a4066f9fc9643d4489fa671c2aa78df153e283f
SaberCon <894876785@qq.com>
2022-12-12 14:03:31 +0800

    Finish elixir largest-series-product

--- elixir/largest-series-product/lib/series.ex
+++ elixir/largest-series-product/lib/series.ex
@@ -3,6 +3,22 @@ defmodule Series do
   Finds the largest product of a given number of consecutive numbers in a given string of numbers.
   """
   @spec largest_product(String.t(), non_neg_integer) :: non_neg_integer
+  def largest_product(_number_string, 0), do: 1
+
+  def largest_product(_number_string, size) when size < 0 do
+    raise ArgumentError
+  end
+
+  def largest_product(number_string, size) when byte_size(number_string) < size do
+    raise ArgumentError
+  end
+
   def largest_product(number_string, size) do
+    number_string
+    |> String.graphemes()
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.chunk_every(size, 1, :discard)
+    |> Stream.map(&Enum.product/1)
+    |> Enum.max()
   end
 end
