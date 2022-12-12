274a9cb89c2d42d2f636d061af495de84c34226e
SaberCon <894876785@qq.com>
2022-12-12 15:49:16 +0800

    Finish elixir meetup

--- elixir/meetup/lib/meetup.ex
+++ elixir/meetup/lib/meetup.ex
@@ -3,6 +3,16 @@ defmodule Meetup do
   Calculate meetup dates.
   """
 
+  @weekdays [
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
+    sunday: 7
+  ]
+
   @type weekday ::
           :monday
           | :tuesday
@@ -22,5 +32,22 @@ defmodule Meetup do
   """
   @spec meetup(pos_integer, pos_integer, weekday, schedule) :: Date.t()
   def meetup(year, month, weekday, schedule) do
+    start =
+      case schedule do
+        :first -> 1
+        :second -> 8
+        :third -> 15
+        :fourth -> 22
+        :last -> Date.days_in_month(Date.new!(year, month, 1)) - 6
+        :teenth -> 13
+      end
+
+    do_meetup(year, month, start, @weekdays[weekday])
+  end
+
+  defp do_meetup(year, month, start, weekday) do
+    start..(start + 6)
+    |> Enum.map(&Date.new!(year, month, &1))
+    |> Enum.find(fn date -> Date.day_of_week(date) == weekday end)
   end
 end
