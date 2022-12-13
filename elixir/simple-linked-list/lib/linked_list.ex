defac02cb8a6d4729881b906ed5d5e9696805d7e
SaberCon <894876785@qq.com>
2022-12-13 08:40:55 +0800

    Finish elixir simple-linked-list

--- elixir/simple-linked-list/lib/linked_list.ex
+++ elixir/simple-linked-list/lib/linked_list.ex
@@ -5,64 +5,57 @@ defmodule LinkedList do
   Construct a new LinkedList
   """
   @spec new() :: t
-  def new() do
-    # Your implementation here...
-  end
+  def new(), do: {}
 
   @doc """
   Push an item onto a LinkedList
   """
   @spec push(t, any()) :: t
-  def push(list, elem) do
-    # Your implementation here...
-  end
+  def push(list, elem), do: {elem, list}
 
   @doc """
   Counts the number of elements in a LinkedList
   """
   @spec count(t) :: non_neg_integer()
-  def count(list) do
-    # Your implementation here...
-  end
+  def count({}), do: 0
+  def count({_, t}), do: 1 + count(t)
 
   @doc """
   Determine if a LinkedList is empty
   """
   @spec empty?(t) :: boolean()
-  def empty?(list) do
-    # Your implementation here...
-  end
+  def empty?({}), do: true
+  def empty?(_), do: false
 
   @doc """
   Get the value of a head of the LinkedList
   """
   @spec peek(t) :: {:ok, any()} | {:error, :empty_list}
-  def peek(list) do
-    # Your implementation here...
-  end
+  def peek({}), do: {:error, :empty_list}
+  def peek({h, _}), do: {:ok, h}
 
   @doc """
   Get tail of a LinkedList
   """
   @spec tail(t) :: {:ok, t} | {:error, :empty_list}
-  def tail(list) do
-    # Your implementation here...
-  end
+  def tail({}), do: {:error, :empty_list}
+  def tail({_, t}), do: {:ok, t}
 
   @doc """
   Remove the head from a LinkedList
   """
   @spec pop(t) :: {:ok, any(), t} | {:error, :empty_list}
-  def pop(list) do
-    # Your implementation here...
-  end
+  def pop({}), do: {:error, :empty_list}
+  def pop({h, t}), do: {:ok, h, t}
 
   @doc """
   Construct a LinkedList from a stdlib List
   """
   @spec from_list(list()) :: t
   def from_list(list) do
-    # Your implementation here...
+    list
+    |> Enum.reduce(new(), &push(&2, &1))
+    |> reverse()
   end
 
   @doc """
@@ -70,14 +63,21 @@ defmodule LinkedList do
   """
   @spec to_list(t) :: list()
   def to_list(list) do
-    # Your implementation here...
+    Stream.unfold(list, fn list ->
+      case pop(list) do
+        {:ok, h, t} -> {h, t}
+        {:error, :empty_list} -> nil
+      end
+    end)
+    |> Enum.to_list()
   end
 
   @doc """
   Reverse a LinkedList
   """
   @spec reverse(t) :: t
-  def reverse(list) do
-    # Your implementation here...
-  end
+  def reverse(list), do: do_reverse(list, {})
+
+  defp do_reverse({}, acc), do: acc
+  defp do_reverse({h, t}, acc), do: do_reverse(t, push(acc, h))
 end
