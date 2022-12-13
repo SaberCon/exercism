4e349f8f4af00f645ae56abe559f62d62c5e08ba
SaberCon <894876785@qq.com>
2022-12-13 08:07:49 +0800

    Finish elixir rational-numbers

--- elixir/rational-numbers/lib/rational_numbers.ex
+++ elixir/rational-numbers/lib/rational_numbers.ex
@@ -5,55 +5,76 @@ defmodule RationalNumbers do
   Add two rational numbers
   """
   @spec add(a :: rational, b :: rational) :: rational
-  def add(a, b) do
+  def add({a1, a2}, {b1, b2}) do
+    {a1 * b2 + b1 * a2, a2 * b2}
+    |> reduce()
   end
 
   @doc """
   Subtract two rational numbers
   """
   @spec subtract(a :: rational, b :: rational) :: rational
-  def subtract(a, b) do
+  def subtract({a1, a2}, {b1, b2}) do
+    add({a1, a2}, {-b1, b2})
   end
 
   @doc """
   Multiply two rational numbers
   """
   @spec multiply(a :: rational, b :: rational) :: rational
-  def multiply(a, b) do
+  def multiply({a1, a2}, {b1, b2}) do
+    {a1 * b1, a2 * b2}
+    |> reduce()
   end
 
   @doc """
   Divide two rational numbers
   """
-  @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by(num, den) do
+  @spec divide_by(a :: rational, b :: rational) :: rational
+  def divide_by({a1, a2}, {b1, b2}) when b1 != 0 do
+    multiply({a1, a2}, {b2, b1})
   end
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs(a) do
+  def abs({a1, a2}) do
+    {Kernel.abs(a1), Kernel.abs(a2)}
+    |> reduce()
   end
 
   @doc """
   Exponentiation of a rational number by an integer
   """
   @spec pow_rational(a :: rational, n :: integer) :: rational
-  def pow_rational(a, n) do
+  def pow_rational({a1, a2}, n) when n >= 0 do
+    {Integer.pow(a1, n), Integer.pow(a2, n)}
+    |> reduce()
+  end
+
+  def pow_rational({a1, a2}, n) when n < 0 do
+    pow_rational({a2, a1}, -n)
   end
 
   @doc """
   Exponentiation of a real number by a rational number
   """
   @spec pow_real(x :: integer, n :: rational) :: float
-  def pow_real(x, n) do
+  def pow_real(x, {n1, n2}) do
+    x ** (n1 / n2)
   end
 
   @doc """
   Reduce a rational number to its lowest terms
   """
   @spec reduce(a :: rational) :: rational
-  def reduce(a) do
+  def reduce({a1, a2}) when a2 < 0 do
+    reduce({-a1, -a2})
+  end
+
+  def reduce({a1, a2}) do
+    gcd = Integer.gcd(a1, a2)
+    {a1 / gcd, a2 / gcd}
   end
 end
