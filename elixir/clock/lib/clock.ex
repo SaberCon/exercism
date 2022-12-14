c129ef1bee24fccb20690b0d5b7330d62aa41702
SaberCon <894876785@qq.com>
2022-12-14 09:26:22 +0800

    Finish elixir clock

--- elixir/clock/lib/clock.ex
+++ elixir/clock/lib/clock.ex
@@ -8,7 +8,16 @@ defmodule Clock do
       "08:09"
   """
   @spec new(integer, integer) :: Clock
+  def new(hour, minute) when minute < 0 or minute > 59 do
+    new(hour + Integer.floor_div(minute, 60), Integer.mod(minute, 60))
+  end
+
+  def new(hour, minute) when hour < 0 or hour > 23 do
+    new(Integer.mod(hour, 24), minute)
+  end
+
   def new(hour, minute) do
+    %Clock{hour: hour, minute: minute}
   end
 
   @doc """
@@ -19,5 +28,16 @@ defmodule Clock do
   """
   @spec add(Clock, integer) :: Clock
   def add(%Clock{hour: hour, minute: minute}, add_minute) do
+    new(hour, minute + add_minute)
+  end
+
+  defimpl String.Chars, for: Clock do
+    def to_string(%Clock{hour: hour, minute: minute}) do
+      format(hour) <> ":" <> format(minute)
+    end
+
+    defp format(number) do
+      number |> Integer.to_string() |> String.pad_leading(2, "0")
+    end
   end
 end
