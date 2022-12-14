414eba74a80e463967c661a80345fd5c62e05366
SaberCon <894876785@qq.com>
2022-12-14 16:48:26 +0800

    Finish elixir diamond

--- elixir/diamond/lib/diamond.ex
+++ elixir/diamond/lib/diamond.ex
@@ -5,5 +5,22 @@ defmodule Diamond do
   """
   @spec build_shape(char) :: String.t()
   def build_shape(letter) do
+    ?A..(letter - 1)//1
+    |> Enum.concat(letter..?A)
+    |> Enum.map_join(&build_row(&1, ?A, letter))
+  end
+
+  defp build_row(letter, start_letter, end_letter) do
+    first_half =
+      <<letter>>
+      |> String.pad_leading(end_letter - letter + 1)
+      |> String.pad_trailing(end_letter - start_letter + 1)
+
+    second_half =
+      first_half
+      |> String.reverse()
+      |> String.slice(1..-1)
+
+    first_half <> second_half <> "\n"
   end
 end
