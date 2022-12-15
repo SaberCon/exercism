db85ac60b5194642c1d07eb8e394ac3ddf13e74f
SaberCon <894876785@qq.com>
2022-12-15 22:22:05 +0800

    Finish elixir square-root

--- elixir/square-root/lib/square_root.ex
+++ elixir/square-root/lib/square_root.ex
@@ -3,6 +3,19 @@ defmodule SquareRoot do
   Calculate the integer square root of a positive integer
   """
   @spec calculate(radicand :: pos_integer) :: pos_integer
-  def calculate(radicand) do
+  def calculate(radicand), do: square_root(radicand, 1, radicand)
+
+  defp square_root(_radicand, mi, ma) when mi > ma do
+    raise ArgumentError
+  end
+
+  defp square_root(radicand, mi, ma) do
+    mid = floor((mi + ma) / 2)
+
+    case mid * mid do
+      ^radicand -> mid
+      square when square > radicand -> square_root(radicand, mi, mid - 1)
+      square when square < radicand -> square_root(radicand, mid + 1, ma)
+    end
   end
 end
