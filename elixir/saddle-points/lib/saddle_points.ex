b2a7fa3108bab9defa7558ef6340e57fc63c1e0a
SaberCon <894876785@qq.com>
2022-12-15 11:35:41 +0800

    Finish elixir saddle-points

--- elixir/saddle-points/lib/saddle_points.ex
+++ elixir/saddle-points/lib/saddle_points.ex
@@ -5,7 +5,12 @@ defmodule SaddlePoints do
   """
   @spec rows(String.t()) :: [[integer]]
   def rows(str) do
+    str
+    |> String.split("\n", trim: true)
+    |> Enum.map(&row/1)
   end
+
+  defp row(line), do: line |> String.split() |> Enum.map(&String.to_integer/1)
 
   @doc """
   Parses a string representation of a matrix
@@ -13,6 +18,8 @@ defmodule SaddlePoints do
   """
   @spec columns(String.t()) :: [[integer]]
   def columns(str) do
+    rows(str)
+    |> Enum.zip_with(& &1)
   end
 
   @doc """
@@ -21,5 +28,9 @@ defmodule SaddlePoints do
   """
   @spec saddle_points(String.t()) :: [{integer, integer}]
   def saddle_points(str) do
+    for {row, x} <- Enum.with_index(rows(str), 1),
+        {col, y} <- Enum.with_index(columns(str), 1),
+        Enum.max(row) == Enum.min(col),
+        do: {x, y}
   end
 end
