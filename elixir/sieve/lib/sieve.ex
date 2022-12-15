fdbcdb8da09892a1ed278d025a41a925b00663ad
SaberCon <894876785@qq.com>
2022-12-15 19:24:23 +0800

    Finish elixir sieve

--- elixir/sieve/lib/sieve.ex
+++ elixir/sieve/lib/sieve.ex
@@ -4,5 +4,15 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
+    2..limit//1
+    |> Enum.to_list()
+    |> sieve(limit, [])
+  end
+
+  defp sieve([], _limit, acc), do: acc |> Enum.reverse()
+
+  defp sieve([prime | rest], limit, acc) do
+    composites = prime..limit//prime |> Enum.to_list()
+    sieve(rest -- composites, limit, [prime | acc])
   end
 end
