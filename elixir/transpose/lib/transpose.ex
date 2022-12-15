58f0771bb0dad0ab0c74049388a972dfbe026571
SaberCon <894876785@qq.com>
2022-12-15 23:41:48 +0800

    Finish elixir transpose

--- elixir/transpose/lib/transpose.ex
+++ elixir/transpose/lib/transpose.ex
@@ -19,5 +19,32 @@ defmodule Transpose do
 
   @spec transpose(String.t()) :: String.t()
   def transpose(input) do
+    input
+    |> String.split("\n")
+    |> pad_short_rows()
+    |> rows_to_columns()
+    |> Enum.map_join("\n", &normalize_column/1)
+  end
+
+  defp pad_short_rows(rows) do
+    max_length =
+      rows
+      |> Enum.map(&String.length/1)
+      |> Enum.max()
+
+    rows
+    |> Enum.map(&String.pad_trailing(&1, max_length, "🥵"))
+  end
+
+  defp normalize_column(rows) do
+    rows
+    |> String.trim_trailing("🥵")
+    |> String.replace("🥵", " ")
+  end
+
+  defp rows_to_columns(rows) do
+    rows
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.zip_with(&Enum.join/1)
   end
 end
