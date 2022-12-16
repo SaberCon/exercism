459b8e6e1a1788b2ab9ac6b47a21c760f5be3d65
SaberCon <894876785@qq.com>
2022-12-16 14:24:29 +0800

    Finish elixir list-ops

--- elixir/list-ops/lib/list_ops.ex
+++ elixir/list-ops/lib/list_ops.ex
@@ -7,34 +7,39 @@ defmodule ListOps do
 
   @spec count(list) :: non_neg_integer
   def count(l) do
+    foldl(l, 0, fn _, acc -> acc + 1 end)
   end
 
   @spec reverse(list) :: list
   def reverse(l) do
+    foldl(l, [], fn x, acc -> [x | acc] end)
   end
 
   @spec map(list, (any -> any)) :: list
   def map(l, f) do
+    foldr(l, [], fn x, acc -> [f.(x) | acc] end)
   end
 
   @spec filter(list, (any -> as_boolean(term))) :: list
   def filter(l, f) do
+    foldr(l, [], fn x, acc -> if(f.(x), do: [x | acc], else: acc) end)
   end
 
   @type acc :: any
   @spec foldl(list, acc, (any, acc -> acc)) :: acc
-  def foldl(l, acc, f) do
-  end
+  def foldl([], acc, _f), do: acc
+  def foldl([h | t], acc, f), do: foldl(t, f.(h, acc), f)
 
   @spec foldr(list, acc, (any, acc -> acc)) :: acc
-  def foldr(l, acc, f) do
-  end
+  def foldr(l, acc, f), do: foldl(reverse(l), acc, f)
 
   @spec append(list, list) :: list
   def append(a, b) do
+    foldr(a, b, fn x, acc -> [x | acc] end)
   end
 
   @spec concat([[any]]) :: [any]
   def concat(ll) do
+    foldr(ll, [], &append/2)
   end
 end
