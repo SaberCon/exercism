246ae43ebb33b05bca2c30619a24896b217e9c8b
SaberCon <894876785@qq.com>
2022-12-18 11:51:33 +0800

    Finish elixir rail-fence-cipher

--- elixir/rail-fence-cipher/lib/rail_fence_cipher.ex
+++ elixir/rail-fence-cipher/lib/rail_fence_cipher.ex
@@ -4,6 +4,8 @@ defmodule RailFenceCipher do
   """
   @spec encode(String.t(), pos_integer) :: String.t()
   def encode(str, rails) do
+    str
+    |> sort_string_by(rail_stream(rails))
   end
 
   @doc """
@@ -11,5 +13,28 @@ defmodule RailFenceCipher do
   """
   @spec decode(String.t(), pos_integer) :: String.t()
   def decode(str, rails) do
+    1..String.length(str)
+    |> sort_by(rail_stream(rails))
+    |> then(&sort_string_by(str, &1))
+  end
+
+  defp rail_stream(rails) do
+    1..rails//1
+    |> Enum.concat((rails - 1)..2//-1)
+    |> Stream.cycle()
+  end
+
+  defp sort_by(enumerable, indices) do
+    enumerable
+    |> Enum.zip(indices)
+    |> Enum.sort_by(&elem(&1, 1))
+    |> Enum.map(&elem(&1, 0))
+  end
+
+  defp sort_string_by(str, indices) do
+    str
+    |> String.graphemes()
+    |> sort_by(indices)
+    |> Enum.join()
   end
 end
