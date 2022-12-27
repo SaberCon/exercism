309249d940d2133f24a1f52dcb78d1906285601b
SaberCon <894876785@qq.com>
2022-12-27 12:53:16 +0800

    Finish elixir react

--- elixir/react/lib/react.ex
+++ elixir/react/lib/react.ex
@@ -1,4 +1,6 @@
 defmodule React do
+  defstruct values: %{}, outputs: %{}, callbacks: %{}
+
   @opaque cells :: pid
 
   @type cell :: {:input, String.t(), any} | {:output, String.t(), [String.t()], fun()}
@@ -8,6 +10,28 @@ defmodule React do
   """
   @spec new(cells :: [cell]) :: {:ok, pid}
   def new(cells) do
+    Agent.start_link(fn -> init(%__MODULE__{}, cells) end)
+  end
+
+  defp init(state, []), do: state
+
+  defp init(state, [{:input, name, value} | rest]) do
+    put_in(state.values[name], value)
+    |> init(rest)
+  end
+
+  defp init(state, [{:output, name, dependencies, fun} | rest]) do
+    put_in(state.outputs[name], {dependencies, fun})
+    |> then(fn state -> put_in(state.values[name], calculate(state, name)) end)
+    |> init(rest)
+  end
+
+  defp calculate(state, name) do
+    {dependencies, fun} = state.outputs[name]
+
+    dependencies
+    |> Enum.map(&state.values[&1])
+    |> then(&apply(fun, &1))
   end
 
   @doc """
@@ -15,6 +39,7 @@ defmodule React do
   """
   @spec get_value(cells :: pid, cell_name :: String.t()) :: any()
   def get_value(cells, cell_name) do
+    Agent.get(cells, fn state -> state.values[cell_name] end)
   end
 
   @doc """
@@ -22,6 +47,35 @@ defmodule React do
   """
   @spec set_value(cells :: pid, cell_name :: String.t(), value :: any) :: :ok
   def set_value(cells, cell_name, value) do
+    Agent.update(cells, fn state ->
+      update_value({state, []}, cell_name, value) |> elem(0)
+    end)
+  end
+
+  defp update_value({state, updated_values}, cell_name, value) do
+    if state.values[cell_name] == value do
+      {state, updated_values}
+    else
+      state.callbacks
+      |> Map.get(cell_name, %{})
+      |> Enum.each(fn {callback_name, callback} -> callback.(callback_name, value) end)
+
+      new_updated_values = [cell_name | updated_values]
+      new_state = put_in(state.values[cell_name], value)
+
+      state.outputs
+      |> Stream.filter(fn {_, {dependencies, _}} ->
+        cell_name in dependencies and Enum.all?(dependencies, &(&1 in new_updated_values))
+      end)
+      |> Stream.map(fn {name, _} -> name end)
+      |> Enum.reduce({new_state, new_updated_values}, fn name, {state, updated_values} ->
+        update_value({state, updated_values}, name)
+      end)
+    end
+  end
+
+  defp update_value({state, updated_values}, cell_name) do
+    update_value({state, updated_values}, cell_name, calculate(state, cell_name))
   end
 
   @doc """
@@ -34,6 +88,15 @@ defmodule React do
           callback :: fun()
         ) :: :ok
   def add_callback(cells, cell_name, callback_name, callback) do
+    Agent.update(cells, fn state ->
+      update_in(state.callbacks, &do_add_callback(&1, cell_name, callback_name, callback))
+    end)
+  end
+
+  defp do_add_callback(callbacks, cell_name, callback_name, callback) do
+    callbacks
+    |> Map.put_new(cell_name, %{})
+    |> put_in([cell_name, callback_name], callback)
   end
 
   @doc """
@@ -41,5 +104,14 @@ defmodule React do
   """
   @spec remove_callback(cells :: pid, cell_name :: String.t(), callback_name :: String.t()) :: :ok
   def remove_callback(cells, cell_name, callback_name) do
+    Agent.update(cells, fn state ->
+      update_in(state.callbacks, &do_remove_callback(&1, cell_name, callback_name))
+    end)
+  end
+
+  defp do_remove_callback(callbacks, cell_name, callback_name) do
+    callbacks
+    |> pop_in([cell_name, callback_name])
+    |> elem(1)
   end
 end
